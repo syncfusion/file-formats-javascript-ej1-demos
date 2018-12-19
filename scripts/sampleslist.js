ae32c103fee360836df2368d6c6a480a2940d0bd
build <buildautomation@syncfusion.com>
2018-12-19 01:35:56 -0800

    v16.4.0.42

---
+++ scripts/sampleslist.js
@@ -0,0 +1,107 @@
+window.GroupingList = ["FILE FORMATS"];
+window.SamplesList = [
+	{
+    "name": "XlsIO", 
+	"id": "xlsio", 
+	"Group": "FILE FORMATS", 
+	"childcount": "1",
+	"querystring": "CreateSpreadsheet", 
+	"liveediturl": "", 
+	"isResponsive": "true",
+    "ugurl": "https://help.syncfusion.com/file-formats/xlsio/overview",
+    "forumurl": "https://www.syncfusion.com/forums/aspnetmvc/xlsiO",
+    "kburl": "https://www.syncfusion.com/kb/aspnetmvc/XlsIO",
+    "samples": [
+          { "id": "1", "name": "Create Spreadsheet", "querystring": "CreateSpreadsheet", "liveediturl": "","childcount": "0" },
+		  { "id": "2", "name": "Formulas", "querystring": "Formulas", "liveediturl": "","childcount": "0" },
+		  { "id": "3", "name": "Embedded Chart", "querystring": "EmbeddedChart", "liveediturl": "","childcount": "0" },
+		   { "id": "4", "name": "Import Data", "querystring": "ImportData", "liveediturl": "","childcount": "0" },
+		  { "id": "5", "name": "Template Marker", "querystring": "TemplateMarker", "liveediturl": "","childcount": "0" },
+    ]
+},
+	{
+    "name": "Presentation",
+        "id": "presentation",
+        "Group": "FILE FORMATS",
+        "childcount": "1",
+		"type": "update",
+        "liveediturl": "",
+        "querystring": "Slides",
+        "isResponsive": "true",
+        "ugurl": "http://help.syncfusion.com/js/presentation/overview",
+        "forumurl": "https://www.syncfusion.com/forums/javascript/presentation",
+        "kburl": "https://www.syncfusion.com/kb/javascript/ejpresentation",
+        "samples": [
+              { "id": "1", "name": "Slides", "querystring": "Slides", "childcount": "0" },
+              { "id": "2", "name": "Manipulating SmartArt", "querystring": "ManipulatingSmartArt", "childcount": "0" },
+              { "id": "3", "name": "Merging Presentation", "querystring": "MergingPresentation", "childcount": "0" },
+              { "id": "4", "name": "Notes", "querystring": "Notes", "childcount": "0" },
+              { "id": "5", "name": "PPTX To PDF", "querystring": "PPTXToPDF", "childcount": "0" },
+			  { "id": "6", "name": "Comments", "querystring": "Comments", "childcount": "0"},
+			  { "id": "7", "name": "Connectors", "querystring": "Connectors", "childcount": "0", "type": "new"},
+			  { "id": "8", "name": "Create Animation", "querystring": "CreateAnimation", "childcount": "0" },
+		      { "id": "9", "name": "Modify Animation", "querystring": "ModifyAnimation", "childcount": "0" },
+			  { "id": "10", "name": "Slide Transition", "querystring": "SlideTransition", "childcount": "0" }
+        ]
+},
+{
+    "name": "PDF",
+        "id": "pdf",
+        "Group": "FILE FORMATS",
+        "childcount": "1",
+        "liveediturl": "",
+        "querystring": "Getting Started",        
+        "isResponsive": "true",		
+       "ugurl": "https://help.syncfusion.com/file-formats/PDF/overview",
+       "forumurl": "https://www.syncfusion.com/forums/aspnetmvc/PDF",
+       "kburl": "https://www.syncfusion.com/kb/aspnetmvc/PDF",
+        "samples": [
+              { "id": "1", "name": "Getting Started", "querystring": "GettingStarted", "childcount": "0" },
+              { "id": "2", "name": "Form Filling SmartArt", "querystring": "FormFill", "childcount": "0" },
+              { "id": "3", "name": "Interactive Features", "querystring": "InteractiveFeature", "childcount": "0" },
+              { "id": "4", "name": "Security", "querystring": "Encryption", "childcount": "0" },
+              { "id": "5", "name": "Table Features", "querystring": "TableFeature", "childcount": "0" }
+        ]
+},
+
+    {
+        "name": "PDF Viewer",
+        "id": "pdfviewer",
+        "childcount": "1",
+        "querystring": "Getting Started",
+        "Group": "FILE FORMATS",
+        "isResponsive": "true",
+        "ugurl": "http://help.syncfusion.com/js/pdfviewer/overview",
+        "forumurl": "https://www.syncfusion.com/forums/javascript/ejpdfviewer",
+        "kburl": "https://www.syncfusion.com/kb/javascript/ejpdfviewer",
+        "samples": [
+            {
+                "id": "1", "name": "Getting Started", "querystring": "GettingStarted", "liveediturl": "Sync_a0x4f2x1", "childcount": "0"
+            },
+			{
+			    "id": "2", "name": "Localization", "querystring": "Localization", "liveediturl": "Sync_i5hve1c3", "childcount": "0"
+			},
+            {
+                "id": "3", "name": "Form Filling", "querystring": "FormFilling", "liveediturl": "Sync_a0x4f2x1", "childcount": "0"
+            }
+        ]
+    },
+    {
+        "name": "DocIO",
+        "id": "docio",
+        "Group": "FILE FORMATS",
+        "childcount": "1",
+        "querystring": "Sales Invoice",
+        "isResponsive": "true",
+        "ugurl": "http://help.syncfusion.com/js/docio/overview",
+        "forumurl": "https://www.syncfusion.com/forums/aspnetmvc/docio",
+        "kburl": "https://www.syncfusion.com/kb/aspnetmvc/docio",
+		"samples": [
+              { "id": "1", "name": "Sales Invoice", "querystring": "SalesInvoice", "childcount": "0" },
+              { "id": "2", "name": "Hello World", "querystring": "GettingStarted", "childcount": "0" },
+              { "id": "3", "name": "Format Text", "querystring": "FormatText", "childcount": "0" },
+              { "id": "4", "name": "Nested Mail merge", "querystring": "NestedMailMerge", "childcount": "0" },
+              { "id": "5", "name": "Word to PDF", "querystring": "WordToPDF", "childcount": "0" }
+        ]
+    }
+];
